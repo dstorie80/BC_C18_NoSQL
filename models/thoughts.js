927c0a6fb0676712f913cf56dd82668a13b48204
dstorie80 <dstorie80@gmail.com>
2024-04-07 13:37:10 -0500

    Made adjustments to the thoughtsController and thoughtsRoutes

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,42 +1,45 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/date.js');
-
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/date.js");
 
 const thoughtsSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            min: 1,
-            max: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Reactions'
-            }
-        ]
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [
+      {
+        username: {
+          type: String,
+          required: true,
         },
-        id: false
-    }
+        reactionText: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-
-
-const Thoughts = model('Thoughts', thoughtsSchema);
+const Thoughts = model("Thoughts", thoughtsSchema);
 
 module.exports = Thoughts;
